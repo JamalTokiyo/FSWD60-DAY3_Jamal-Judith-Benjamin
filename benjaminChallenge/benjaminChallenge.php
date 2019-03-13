f1e36c46c18fa88d867a4b1ce04e4bcfea9bdf13
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-03-13 11:59:54 +0100

    Add content to benjamin folder

--- benjaminChallenge/benjaminChallenge.php
+++ benjaminChallenge/benjaminChallenge.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 	<?php
-	class Books{
+	/*class Books{
 		public $title;
 		public $genre;
 		public $author;
@@ -25,6 +25,7 @@
 		}
 	}
 	$totalbooks = 0;
+	$bookArray = array();
 	$bookArray[0] = new Books("Harry Potter", "Fantasy", "J.K.R", "20.00");
 	$bookArray[1] = new Books("Harry Pooootter", "Fantasy", "J.K.R", "10.00");
 	$bookArray[2] = new Books("Harry Potter is strange", "Fantasy", "J.K.R", "19.00");
@@ -33,7 +34,64 @@
 		echo $value->createString(); 
 		$totalbooks++;
 	}
-		echo "You have ". $totalbooks. " books in total";
+		echo "You have ". $totalbooks. " books in total";*/
+
+################################################################################################################
+
+	/*class items{
+		public $firstItem;
+		public $secondItem;
+		public $thirdItem;
+
+		function __construct($item1, $item2, $item3){
+			$this->firstItem = $item1;
+			$this->secondItem = $item2;
+			$this->thirdItem = $item3;
+		}
+
+		public function totalItems(){
+			$result = $this->firstItem + $this->secondItem + $this->thirdItem;
+			$count = "You bought the first item ". $this->firstItem. " times!<br>You bought the second item ". $this->secondItem. " times!<br>You bought the third item ". $this->thirdItem. " times!";
+			return array($result, $count);
+		}
+	}
+
+	$itemcount = new items(2, 5, 7);
+	$output = $itemcount->totalItems()[0];
+	$output2 = $itemcount->totalItems()[1];
+	echo $output2. "<br>You bought ". $output. " items in total!"; */
+
+	################################################################################################################
+
+	/*class jobs{
+		public $company;
+	}
+
+	class employee extends jobs{
+		public $firstName;
+		public $lastName;
+		public $position;
+		public $salary;
+
+		function __construct($a, $b, $c, $d, $e){
+			$this->company = $a;
+			$this->firstName = $b;
+			$this->lastName = $c;
+			$this->position = $d;
+			$this->salary = $e;
+		}
+
+		function showDetails(){
+			$result = "Hello my name is ". $this->firstName. " ". $this->lastName. " and i work for \"". $this->company. "\" as a ". $this->position. ", i get ". $this->salary. " per month!";
+			return $result;
+		}
+	}
+	$worker = new employee("Pizza Parlor", "Peter", "Parker", "delivery guy", "1000€");
+	$output = $worker->showDetails();
+	echo $output; */
+
+	#######################################################################################################################
+	
 	?>
 </body>
 </html>
